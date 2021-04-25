5cff8bb53109d900a4527a8d5b3cf6407b0aee8b
Jeremy Epstein <jazepstein@gmail.com>
2021-04-26 09:11:41 +1000

    specify base branch

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -17,6 +17,7 @@ export default class Site extends App {
       authCallbackRoute: '/api/create-github-access-token',
       clientId: process.env.GITHUB_CLIENT_ID,
       baseRepoFullName: process.env.REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+      baseBranch: process.env.BASE_BRANCH,
     })
 
     /**
