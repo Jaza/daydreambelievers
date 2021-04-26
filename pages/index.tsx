b5b397a015611914cba069ed999bd2df97bb6cd3
Jeremy Epstein <jazepstein@gmail.com>
2021-04-27 07:17:34 +1000

    component-ised home page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,12 @@
-import Head from 'next/head'
-/*
- ** Import helpers and GetStaticProps type
- */
 import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
 import {
   useGithubJsonForm,
   useGithubToolbarPlugins,
 } from 'react-tinacms-github'
-import { InlineForm, InlineText } from 'react-tinacms-inline'
 import { usePlugin } from 'tinacms'
 import { GetStaticProps } from 'next'
+import Container from '../components/container'
+import HomeTitle from '../components/home-title'
 
 export default function Home({ file, preview }) {
   const formOptions = {
@@ -17,94 +14,18 @@ export default function Home({ file, preview }) {
     fields: [{ name: 'title', component: 'text' }],
   }
 
-  /*
-   ** Register a JSON Tina Form
-   */
-  const [data, form] = useGithubJsonForm(file, formOptions)
+  const [pageData, form] = useGithubJsonForm(file, formOptions)
   usePlugin(form)
 
   useGithubToolbarPlugins()
 
   return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <main>
-        <InlineForm form={form}>
-          <h1 className="title">
-            <InlineText name="title" />
-          </h1>
-        </InlineForm>
-      </main>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-          color: ${preview && 'red'};
-          text-align: center;
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+    <Container {...pageData}>
+      <HomeTitle form={form} />
+    </Container>
   )
 }
 
-/*
- ** Fetch data with getStaticProps based on 'preview' mode
- */
 export const getStaticProps: GetStaticProps = async function ({
   preview,
   previewData,
