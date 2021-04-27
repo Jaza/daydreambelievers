b6f0dd5e8473838d0da41af368d3f7330d354803
Jeremy Epstein <jazepstein@gmail.com>
2021-04-28 07:10:37 +1000

    fix alt

--- components/hero.tsx
+++ components/hero.tsx
@@ -6,7 +6,7 @@ export default function Hero({ form, title }) {
       <div className="container">
         <div className="row">
           <div className="col-lg-12">
-            <img className="img-responsive img-circle img-lesswidth" src="http://daydreambelievers.greenash.net.au/static/cache/thumbnails/image-content-block/site-logo/FB_IMG_1469839784721-2016-07-30-00-53-04_256x256_fit_85.jpg" alt="{title}" />
+            <img className="img-responsive img-circle img-lesswidth" src="http://daydreambelievers.greenash.net.au/static/cache/thumbnails/image-content-block/site-logo/FB_IMG_1469839784721-2016-07-30-00-53-04_256x256_fit_85.jpg" alt={title} />
             <div className="intro-text">
               <InlineForm form={form}>
                 <span className="name">
