2947c57dbf9285c7d727f86fc6e6b0b5a64317af
Jeremy Epstein <jazepstein@gmail.com>
2021-06-09 09:26:01 +1000

    remove s3-sts-token api endpoint

--- pages/api/s3-sts-token.ts
+++
@@ -1,8 +0,0 @@
-import { s3StsToken } from 'next-tinacms-s3'
-
-export default s3StsToken(
-  process.env.S3_UPLOAD_KEY,
-  process.env.S3_UPLOAD_SECRET,
-  process.env.S3_UPLOAD_REGION,
-  process.env.S3_UPLOAD_BUCKET,
-)
