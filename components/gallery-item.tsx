b5dfa0d9eba4ef5cb174a43a2c5afb0ce4e548b5
Jeremy Epstein <jazepstein@gmail.com>
2021-06-10 15:54:44 +1000

    more gallery fields

--- components/gallery-item.tsx
+++ components/gallery-item.tsx
@@ -3,34 +3,34 @@ import { BlocksControls, InlineImage } from 'react-tinacms-inline'
 
 export function GalleryItem({ index, title }) {
   return (
-    <div className="col-sm-4 portfolio-item">
-      <a href={`#galleryModal${index + 1}`} className="portfolio-link" data-toggle="modal">
-        <div className="caption">
-          <div className="caption-content">
-            <i className="fa fa-search-plus fa-3x" />
-          </div>
+    <a href={`#galleryModal${index + 1}`} className="portfolio-link" data-toggle="modal">
+      <div className="caption">
+        <div className="caption-content">
+          <i className="fa fa-search-plus fa-3x" />
         </div>
-        <InlineImage
-          name="image"
-          parse={media => media.previewSrc}
-          uploadDir={() => '/gallery-item/'}
-        >
-          {props => <img
-            src={props.src}
-            alt={title}
-            className="img-responsive"
-          />}
-        </InlineImage>
-      </a>
-    </div>
+      </div>
+      <InlineImage
+        name="image"
+        parse={media => media.previewSrc}
+        uploadDir={() => '/gallery-item/'}
+      >
+        {props => <img
+          src={props.src}
+          alt={title}
+          className="img-responsive"
+        />}
+      </InlineImage>
+    </a>
   )
 }
 
 export const galleryItemBlock = {
   Component: ({ index, data }) => (
-    <BlocksControls index={index}>
-      <GalleryItem index={index} {...data} />
-    </BlocksControls>
+    <div className="col-sm-4 portfolio-item">
+      <BlocksControls index={index}>
+        <GalleryItem index={index} {...data} />
+      </BlocksControls>
+    </div>
   ),
   template: {
     label: 'Gallery item',
@@ -43,7 +43,22 @@ export const galleryItemBlock = {
         name: 'title',
         label: 'Title',
         component: 'text',
-      }
+      },
+      {
+        name: 'content',
+        label: 'Content',
+        component: 'textarea',
+      },
+      {
+        name: 'eventName',
+        label: 'Event name',
+        component: 'text',
+      },
+      {
+        name: 'dateTaken',
+        label: 'Date taken',
+        component: 'text',
+      },
     ],
   },
 }
